7b00adf900740df6edbe97c3981f6f20d1ee4883
Jeffrey Mew <t-jemew@microsoft.com>
2018-12-07 11:25:54 -0800

    removed localhost from http_functions

--- static/src/http_functions.js
+++ static/src/http_functions.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export function add_task(task) {
-    return axios.post("http://localhost:5000/api/v1/add_task", {
+    return axios.post("/api/v1/add_task", {
         task
     })
 }
 
 export function get_all_tasks() {
-    return axios.get("http://localhost:5000/api/v1/get_all_tasks")
+    return axios.get("/api/v1/get_all_tasks")
 }
