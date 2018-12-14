799bb663a18937a9d3ff8154273578d4fb693c69
Jeffrey Mew <t-jemew@microsoft.com>
2018-12-14 13:37:35 -0800

    removed default env vars

--- envvars.py
+++ envvars.py
@@ -8,7 +8,7 @@ AZ_LOCATION = "centralus"
 def setEnvironmentVariables():
     os.environ["AZ_GROUP"] = AZ_GROUP
     os.environ["AZ_LOCATION"] = AZ_LOCATION
-    os.environ["POSTGRES_SERVER_NAME"] = "asdfasdf" # TODO set this variable
-    os.environ["POSTGRES_ADMIN_USER"] = "asdgasdg" # TODO set this variable
-    os.environ["POSTGRES_ADMIN_PASSWORD"] = "asdgadsg123!" # TODO set this variable
-    os.environ["APP_DB_NAME"] = "asdgasdgads" # TODO set this variable
+    os.environ["POSTGRES_SERVER_NAME"] = "" # TODO set this variable
+    os.environ["POSTGRES_ADMIN_USER"] = "" # TODO set this variable
+    os.environ["POSTGRES_ADMIN_PASSWORD"] = "" # TODO set this variable
+    os.environ["APP_DB_NAME"] = "" # TODO set this variable
